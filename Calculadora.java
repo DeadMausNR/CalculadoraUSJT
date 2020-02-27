8ae5ef396a26978e7806eb6c2c2e55334f1c2e44
Nathan Lima <nathanlima123@gmail.com>
2020-02-27 18:43:39 -0300

    Implementando subtracao

--- Calculadora.java
+++ Calculadora.java
@@ -5,29 +5,29 @@ public class Calculadora {
     private static Scanner sn = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        int opcao = 0;
-
         loop: do {
 
             System.out.println("------------- Calculadora -------------");
-            System.out.println("Escolha o método de cálculo:\n1 - Soma\n0 - Sair");
+            System.out.println("Escolha o metodo de calculo:\n1 - Soma\n2 - Subtracao\n0 - Sair");
 
             try {
                 switch (Integer.parseInt(sn.nextLine())) {
                     case 1:
                         Soma();
                         break;
+                    case 2:
+                        Subtracao();
+                        break;
                     case 0:
                         break loop;
                     default:
-                        System.out.println("A opção não é valida, pressione enter para continuar!");
+                        System.out.println("A opcao nao e valida, pressione enter para continuar!");
                         sn.nextLine();
                         break;
                 }
 
             } catch (NumberFormatException e) {
-                System.out.println("O valor inserido não é válido, pressione enter para continuar!");
+                System.out.println("O valor inserido nao e válido, pressione enter para continuar!");
                 sn.nextLine();
                 continue;
             } catch (Exception e) {
@@ -37,7 +37,7 @@ public class Calculadora {
                 continue;
             }
 
-        } while (opcao == 0);
+        } while (true);
 
         sn.close();
     }
@@ -50,6 +50,18 @@ public class Calculadora {
         int val2 = Integer.parseInt(sn.nextLine());
 
         System.out.println("A soma dos dois valores é: " + (val1 + val2));
+        System.out.println("Pressione enter para voltar para o menu!");
+        sn.nextLine();
+    }
+
+    public static void Subtracao() {
+        System.out.print("Digite o primeiro valor inteiro: ");
+        int val1 = Integer.parseInt(sn.nextLine());
+
+        System.out.print("Digite o segundo valor inteiro: ");
+        int val2 = Integer.parseInt(sn.nextLine());
+
+        System.out.println("A subtracao dos dois valores é: " + (val1 - val2));
         System.out.println("Pressione enter para voltar para o menu!");
         sn.nextLine();
     }
